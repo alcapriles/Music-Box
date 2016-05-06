339e693cdd6614ef8c9419ac32fd93d3d3819b66
alcapriles <alcp.capriles@gmail.com>
2016-05-06 15:17:57 -0300

    Chroma fixes

--- chromagram.py
+++ chromagram.py
@@ -13,12 +13,75 @@ st=2**(1/float(nbin))
 step=128
 fr=11025
 df=4
+        
+tunechroma1=[np.log2(A5*st**i) for i in range(nbin)] 
+tunechroma2=[int(np.log2(A5*st**i)) for i in range(nbin)]
+chroma=np.asarray(tunechroma1)-np.asarray(tunechroma2);
+
+spf = wave.open('background.wav','r')
+
+signal = spf.readframes(-1)
+signal = np.fromstring(signal, 'Int16')
+fs = spf.getframerate()
+
+b = sg.firwin(30, 1.0/df)
+signal = sg.lfilter(b, 1., signal)
+signal = signal.swapaxes(0,-1)[30+1::4].swapaxes(0,-1)
+
+fs = fs / df
+
+step=Nfft-step
+
+Pxx, freqs, bins, im  = pylab.specgram(signal,Fs=fs,window=np.hamming(Nfft),NFFT=Nfft,noverlap=step,Fc=0)
+
+freqs = freqs[1:,]
+
+freqschroma=np.asarray(np.log2(freqs)) - np.asarray([int(np.log2(f)) for f in freqs])
+
+nchroma=len(chroma)
+nfreqschroma=len(freqschroma)
+
+CD=np.zeros((nfreqschroma, nchroma))
+
+for i in range(nchroma):
+    CD[:,i] = np.abs(freqschroma - chroma[i])
+
+FlipMatrix=np.flipud(CD)
+
+min_index = []
+min_value = []
+
+for i in reversed(range(FlipMatrix.shape[0])):
+    index, value = min(enumerate(FlipMatrix[i]), key=operator.itemgetter(1))
+    min_index.append(index)
+    min_value.append(value)
+
+CS = np.zeros((len(chroma),Pxx.shape[1]))
+
+Magnitude= np.log(abs(Pxx[1:,]))
+
+for i in range(CS.shape[0]):
+    a = [index for index,x in enumerate(min_index) if x == i]
+    AIndex = np.zeros((len(a),Pxx.shape[1]))
+
+t = 0    
+for value in a:
+    AIndex[t,:] = Magnitude[value,:]
+    t=t+1
     
+MeanMag=[]
+
+for M in AIndex.T:
+    MeanMag.append(np.mean(M))
+   
+CS[i,:] = MeanMag
+
+CS= CS / CS.max()
+
 colunas = len(CS[1,:])
 notas = "\\relative c' { "
 for j in range(colunas):
     a = []
-      
     for i in range(12):
         if CS[i,j] > 0.95:
             if i == 0:
@@ -57,7 +120,7 @@ for j in range(colunas):
             elif i == 11:
                 a.append('c ')
                 
-        if len(a) >= 3:
+        if len(a) >= 2:
             for u in range (len(a)-1):
                 if len(a) == 3:
                     notas += '<<{} {} {}>>'.format(a[0], a[1], a[2])
@@ -69,69 +132,11 @@ for j in range(colunas):
                     notas+= '<<{} {} {} {} {} {}>>'.format(a[0], a[1], a[2], a[3], a[4], a[5])
                             
                             
-            else:
-                for v in range (len(b)-1):
-                    notas+= a[v]
+        elif (len(a) > 0) and (len(a) < 2):
+            for u in range (len(a)-1):
+                notas += '<<{}>>'.format(a[u])
 notas += '}'
+
+print(a)
 #O programa varre primeiro as colunas da matriz, e para cada linha da coluna selecionada observa-se se possui notas relevantes.
 #A partir disso, verifica-se a presença de acordes, e os adiciona, ou não, na string.
-        
-tunechroma1=[np.log2(A5*st**i) for i in range(nbin)] 
-tunechroma2=[int(np.log2(A5*st**i)) for i in range(nbin)]
-chroma=np.asarray(tunechroma1)-np.asarray(tunechroma2);
-
-spf = wave.open('output.wav','r')
-
-signal = spf.readframes(-1)
-signal = np.fromstring(signal, 'Int16')
-fs = spf.getframerate()
-
-b = sg.firwin(30, 1.0/df)
-signal = sg.lfilter(b, 1., signal)
-signal = signal.swapaxes(0,-1)[30+1::4].swapaxes(0,-1)
-
-fs = fs / df
-
-step=Nfft-step
-
-Pxx, freqs, bins, im  = pylab.specgram(signal,Fs=fs,window=np.hamming(Nfft),NFFT=Nfft,noverlap=step,Fc=0)
-
-freqs = freqs[1:,]
-
-freqschroma=np.asarray(np.log2(freqs)) - np.asarray([int(np.log2(f)) for f in freqs])
-
-nchroma=len(chroma)
-nfreqschroma=len(freqschroma)
-
-CD=np.zeros((nfreqschroma, nchroma))
-
-for i in range(nchroma):
-CD[:,i] = np.abs(freqschroma - chroma[i])
-
-FlipMatrix=np.flipud(CD)
-
-min_index = []
-min_value = []
-
-for i in reversed(range(FlipMatrix.shape[0])):
-index, value = min(enumerate(FlipMatrix[i]), key=operator.itemgetter(1))
-min_index.append(index)
-min_value.append(value)
-
-CS = np.zeros((len(chroma),Pxx.shape[1]))
-
-Magnitude= np.log(abs(Pxx[1:,]))
-
-for i in range(CS.shape[0]):
-a = [index for index,x in enumerate(min_index) if x == i]
-AIndex = np.zeros((len(a),Pxx.shape[1]))
-t=0;
-for value in a:
-    AIndex[t,:] = Magnitude[value,:]
-    t=t+1
-MeanMag=[]
-for M in AIndex.T:
-    MeanMag.append(np.mean(M))
-CS[i,:] = MeanMag
-
-CS= CS / CS.max()
