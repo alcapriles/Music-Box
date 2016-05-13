7f4029517351a7de6f49c2bdd6e78e168ee71268
VictorLMoura <victorlaperutademoura@hotmail.com>
2016-05-13 14:19:06 -0300

    -

--- GUI_2.py
+++ GUI_2.py
@@ -1,51 +1,61 @@
-from tkinter import Tk, Frame, Checkbutton
-from tkinter import BooleanVar, BOTH
 import tkinter as tk
+import os
+import subprocess
+import lilypond_generator
 
-class Example(Frame):
-  
-    def __init__(self, parent):
-        Frame.__init__(self, parent)   
-         
-        self.parent = parent
+class Example:
+    def __init__(self):
+        self.lilypond = lilypond_generator.Lilypond()
+        self.arquivo = self.lilypond.arquivo
+        self.button_status = 'pause'        
         
-        self.initUI()
-        
-        
-    def initUI(self):
         self.window = tk.Tk()
-        self.window.title("Jogo da Velha!")
-        self.window.geometry("600x400")
-        self.window.rowconfigure(0, minsize=100,weight=1)
-        self.window.columnconfigure(0, minsize=100,weight=1)
+        self.window.title('Music Box')
+        self.window.geometry('800x300')
+        
+        self.window.rowconfigure(0, minsize=170,weight=1)
+        self.window.rowconfigure(1, minsize=70, weight=1)
+        self.window.rowconfigure(2, minsize=70, weight=1)
+        self.window.columnconfigure(0, minsize=50,weight=1)
         self.window.columnconfigure(1, minsize=100,weight=1)
-      
-        self.parent.title("Checkbutton")
-
-        self.pack(fill=BOTH, expand=True)
-        self.var = BooleanVar()
+        self.window.columnconfigure(2, minsize=200,weight=1)
+        self.window.columnconfigure(3, minsize=450, weight=1)        
         
-        cb = Checkbutton(self, text="Show title",
-            variable=self.var, command=self.onClick)
-        cb.select()
-        cb.place(x=50, y=50)
+        self.play = tk.PhotoImage(file = 'Icone_play.png')
+        self.pause = tk.PhotoImage(file = 'Icone_pause.png')
+        self.button_gravar = tk.Button(self.window, image = self.pause)
+        self.button_gravar.grid(row=1, column= 1, sticky= 'nsew') 
+        self.button_gravar.configure(text= 'Pause')
+        self.button_gravar.configure(command= self.play_pause)  
         
-
-    def onClick(self):
-       
-        if self.var.get() == True:
-            self.master.title("Checkbutton")
-        else:
-            self.master.title("")
-
-
-def main():
-  
-    root = Tk()
-    root.geometry("250x150+300+300")
-    app = Example(root)
-    root.mainloop()  
-
-
-if __name__ == '__main__':
-    main()  
+        self.label_partitura = tk.Label(self.window)
+        self.label_partitura.configure(font= 'Courirer 16 bold')
+        self.label_partitura.configure(text= 'Arquivo gerado com a partitura')
+        self.label_partitura.grid(row=2, column= 3, sticky= 'nsew')
+        
+        self.label_arquivo = tk.Label(self.window)
+        self.label_arquivo.configure(font= 'Courirer 14')
+        self.label_arquivo.configure(text= '')
+        self.label_arquivo.grid(row=1, column= 3, sticky= 'nsew')
+        
+    def iniciar(self):
+        self.window.mainloop()
+    
+    def play_pause(self):
+        
+        if (self.button_status == 'pause'):
+            self.button_gravar = tk.Button(self.window, image = self.play)
+            self.button_status = 'play'
+            print('1')
+        elif (self.button_status == 'play'):
+            self.button_gravar = tk.Button(self.window, image = self.pause)
+            self.button_status = 'pause'
+            subprocess.call([r'C:\Users\Victor\Desktop\Projeto_DesSoft\Music-Box', 'chromagram.py'])
+            self.label_arquivo.configure(text = self.arquivo)
+            print('2')
+            
+            
+    
+        
+app= Example()
+app.iniciar()
