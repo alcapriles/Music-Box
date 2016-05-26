aafc36282f946b29c19efdc31589ee0f791a163c
VictorLMoura <victorlaperutademoura@hotmail.com>
2016-05-26 17:15:57 -0300

    Arrumei um pouco mais a GUI

--- ana_GUI.py
+++ ana_GUI.py
@@ -21,32 +21,32 @@ class App:
         
         self.window = tk.Tk()
         self.window.title('Music Box')
-        self.window.geometry("400x400")
-        self.window.rowconfigure(0, minsize=200)
-        self.window.rowconfigure(1, minsize=200)
+        self.window.geometry("400x200")
+        self.window.rowconfigure(0, minsize=100)
+        self.window.rowconfigure(1, minsize=100)
         self.window.columnconfigure(0, minsize=200)
         self.window.columnconfigure(1, minsize=200)
 
         
         botao = tk.Button(self.window)
-        botao.configure(text='Mostrar Partitura')
+        botao.configure(text='Mostrar Partitura', font="Times 14 bold")
         botao.configure(command=lilypond_generator.abrir_partitura)
-        botao.grid(row=0, column=0)
+        botao.grid(row=0, column=0, sticky ='nsew')
         
         botao2 = tk.Button(self.window)
-        botao2.configure(text='Gerar Partitura')
+        botao2.configure(text='Gerar Partitura', font="Times 14 bold")
         botao2.configure(command=self.botao_chromagram_clicado)
-        botao2.grid(row=0, column=1)
+        botao2.grid(row=0, column=1, sticky ='nsew')
         
         botao3 = tk.Button(self.window)
-        botao3.configure(text='Gravar')
+        botao3.configure(text='Gravar', font="Times 14 bold")
         botao3.configure(command=recorder.record)
-        botao3.grid(row=1, column=0)
+        botao3.grid(row=1, column=0, sticky ='nsew')
         
         botao4 = tk.Button(self.window)
-        botao4.configure(text='Escolher arquivo')
+        botao4.configure(text='Escolher arquivo', font="Times 14 bold")
         botao4.configure(command = self.escolher_arquivo)
-        botao4.grid(row=1, column=1)
+        botao4.grid(row=1, column=1, sticky ='nsew')
         
     def botao_chromagram_clicado(self):
         C = chroma.make_chroma()
